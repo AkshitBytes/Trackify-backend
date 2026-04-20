02136e37af577b998b19fcd174b6a3722264d4ff
AkshitBytes <akshitKumar129@gmail.com>
2026-04-21 01:27:40 +0530

    fix cors issue

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000'],
+  origin: ['http://localhost:5173', 'http://localhost:3000','https://trackify-d7c74zkxt-akshits-projects-55a8a0fd.vercel.app'],
   credentials: true,
 }));
 app.use(express.json({ limit: '10mb' }));
